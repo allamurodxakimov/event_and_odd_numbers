1ebb9f2633913e3823bfcb62ebd9810f641aaf3f
allamurodxakimov <xakimov2806@gmail.com>
2023-09-25 18:07:08 -0700

    sum even

--- sum_even.py
+++ sum_even.py
@@ -1,6 +1,15 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var = int(input())
 #Create a variable "sum_even" and assign it 0.
-
+sum = 0
 #Find the sum of the even digits in the variable "var_int".
+if((var//1000)%2==0):
+    sum+=(var//1000)
+if((var%1000//100)%2==0):
+    sum+=(var%1000//100)
+if((var%1000%100//10)%2==0):
+    sum+=(var%1000%100//10)
+if((var%10)%2==0):
+    sum+=(var%10)
+print(sum)
